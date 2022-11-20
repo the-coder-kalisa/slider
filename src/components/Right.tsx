480db6ce70b502789d4886233458de7d1386041d
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-20 17:15:00 +0200

    finished

--- src/components/Right.tsx
+++ src/components/Right.tsx
@@ -1,21 +1,21 @@
 import { FC } from "react";
 import Person1 from "../images/last.png";
-import Person2 from "../images/another.jpg";
+import Person2 from "../images/another.png";
 import Person3 from "../images/second.png";
 
 const Right: FC = () => {
   return (
     <div className="flex flex-col gap-10 items-center">
       <div>
-        <div className="font-bold text-3xl">Yonetim Kadrosu</div>
-        <div className="max-w-[16rem] text-lg">
+        <div className="font-bold text-2xl">Yonetim Kadrosu</div>
+        <div className="max-w-[16rem]">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit.
         </div>
       </div>
       <div className="flex gap-1">
-        <img className="w-[12rem]" src={Person2} />
-        <img className="w-[12rem]" src={Person3} />
-        <img className="w-[12rem]" src={Person1} />
+        <img className="w-[10rem]" src={Person2} />
+        <img className="w-[10rem]" src={Person3} />
+        <img className="w-[10rem]" src={Person1} />
       </div>
     </div>
   );
